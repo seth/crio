48280f77424cc4e521ad03086e35130194d99def
Seth Falcon <seth@userprimary.net>
2010-03-29 16:31:07 -0700

    Fix double free bug crio_stream_free doesn't touch filter
    The filter pointer is passed in on stream construction and should not
    be free'd by crio_stream_free.

--- src/crio/crio.c
+++ src/crio/crio.c
@@ -56,11 +56,6 @@ void crio_stream_free(struct crio_stream *stream)
 {
     if (!stream) return;
     if (stream->filename) free(stream->filename);
-    if (stream->filter) {
-        crio_list_free(CRIO_LIST(stream->filter), 0);
-        free(stream->filter);
-        stream->filter = NULL;
-    }
     free(stream);
     stream = NULL;
 }
