20f8f6323e8cff458ef7a6751d9d3ea339443b88
Seth Falcon <seth@userprimary.net>
2010-03-30 13:04:26 -0700

    Add some comments to demo test package C code

--- tests/criodemo/src/criodemo.c
+++ tests/criodemo/src/criodemo.c
@@ -2,10 +2,25 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
+/* To use the crio C API, you need to include this "header" file in
+   the source file where you define the R_init_yourpackage package
+   initialization function.
+
+   The entry points in crio are made available via the
+   R_RegisterCCallable/R_GetCCallable mechanism.  To make this more
+   convenient, the crio_stubs.c code declares the needed function
+   pointers and the crio_initialize_stubs function initializes them by
+   making the appropriate calls to R_GetCCallable.
+  */
 #include <crio_stubs.c>
 
 void R_init_criodemo(DllInfo *info)
 {
+    /* This function is defined in crio_stubs.c, see the above include
+     * directive.  It uses teh R_GetCCallable mechanism to initialize
+     * function pointers that you will be able to use in your code to
+     * access the crio API.
+     */
     crio_initialize_stubs();
 }
 
